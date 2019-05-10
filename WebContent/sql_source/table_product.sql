cd69926d38a8799d70a5be2863fc76b93853062f
He, Liu <lhe@emich.edu>
2019-05-10 15:13:14 -0400

    v1.3 add hibernate c3p0 connection

--- WebContent/sql_source/table_product.sql
+++ WebContent/sql_source/table_product.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS product;
 CREATE TABLE product (
-	id int DEFAULT NULL,
+	id int PRIMARY KEY AUTO_INCREMENT,
 	name varchar(50) DEFAULT NULL,
 	price float DEFAULT NULL
 );
--- src/com/cart/util/HibernateUtil.java
+++ src/com/cart/util/HibernateUtil.java
@@ -8,6 +8,7 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
 
 import com.cart.model.Product;
 
@@ -16,44 +17,50 @@ import com.cart.model.Product;
  *
  */
 public class HibernateUtil {
-   private static StandardServiceRegistry registry;
-   private static SessionFactory sessionFactory;
+	private static StandardServiceRegistry registry;
+	private static SessionFactory sessionFactory;
 
-   public static SessionFactory getSessionFactory() {
-      if (sessionFactory == null) {
-         try {
-            StandardServiceRegistryBuilder registryBuilder = 
-                  new StandardServiceRegistryBuilder();
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
 
-            Map<String, String> settings = new HashMap<>();
-            settings.put("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-            settings.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/cart?serverTimezone=UTC");
-            settings.put("hibernate.connection.username", "root");
-            settings.put("hibernate.connection.password", "root");
-            settings.put("hibernate.show_sql", "true");
-            settings.put("hibernate.hbm2ddl.auto", "update");
+				Map<String, Object> settings = new HashMap<>();
+				settings.put("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+				settings.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/cart?serverTimezone=UTC");
+				settings.put("hibernate.connection.username", "root");
+				settings.put("hibernate.connection.password", "root");
+				settings.put("hibernate.show_sql", "true");
+				settings.put("hibernate.hbm2ddl.auto", "update");
 
-            registry = registryBuilder.applySettings(settings).build();
+				// c3p0 configuration
+				settings.put(Environment.C3P0_MIN_SIZE, 5); // Minimum size of pool
+				settings.put(Environment.C3P0_MAX_SIZE, 20); // Maximum size of pool
+				settings.put(Environment.C3P0_ACQUIRE_INCREMENT, 1);// Number of connections acquired at a time when
+																	// pool is exhausted
+				settings.put(Environment.C3P0_TIMEOUT, 1800); // Connection idle time
+				settings.put(Environment.C3P0_MAX_STATEMENTS, 150); // PreparedStatement cache size
 
-            MetadataSources sources = new MetadataSources(registry)
-                  .addAnnotatedClass(Product.class);                  
+				registry = registryBuilder.applySettings(settings).build();
 
-            Metadata metadata = sources.getMetadataBuilder().build();
+				MetadataSources sources = new MetadataSources(registry).addAnnotatedClass(Product.class);
 
-            sessionFactory = metadata.getSessionFactoryBuilder().build();
-         } catch (Exception e) {
-            System.out.println("SessionFactory creation failed");
-            if (registry != null) {
-               StandardServiceRegistryBuilder.destroy(registry);
-            }
-         }
-      }
-      return sessionFactory;
-   }
+				Metadata metadata = sources.getMetadataBuilder().build();
 
-   public static void shutdown() {
-      if (registry != null) {
-         StandardServiceRegistryBuilder.destroy(registry);
-      }
-   }
+				sessionFactory = metadata.getSessionFactoryBuilder().build();
+			} catch (Exception e) {
+				System.out.println("SessionFactory creation failed");
+				if (registry != null) {
+					StandardServiceRegistryBuilder.destroy(registry);
+				}
+			}
+		}
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		if (registry != null) {
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
 }
